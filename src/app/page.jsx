b2b0075a8b8e2ae347f134f66f884b0c1a52ed9c
UzairAwanCode <githubprojects000@gmail.com>
2025-01-14 20:29:43 +0500

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+"use client"
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1>Homepage</h1>
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+"use client"
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul className='flex justify-between m-10 items-center'>
+                <div>
+                    <Link href="/">
+                        <li>Home</li>
+                    </Link>
+                </div>
+                <div className="flex gap-10">
+                    <Link href="/dashboard">
+                        <li>Dashboard</li>
+                    </Link>
+                    <Link href="/login">
+                        <li>Login</li>
+                    </Link>
+                    <Link href="/register">
+                        <li>Register</li>
+                    </Link>
+                </div>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
